d8861f60112c2a1a0aafa0e4cd33b4f195d810c8
mauro soppelsa <mauro_soppelsa@hotmail.com>
2015-08-25 12:21:48 -0300

    code format

--- ex1.js
+++ ex1.js
@@ -1,3 +1,6 @@
+
+/*Vehicle general object*/
+
 function  Vehicle(type_of_vehicle,the_speed){
 
 var type= type_of_vehicle;
@@ -8,7 +11,7 @@ switch(type_of_vehicle){
 
 case "land":
 
-alert("--Land Vehicle--");
+  alert("--Land Vehicle--");
 
 var frontWheels = parseInt(prompt("Enter the radius for the front wheels"));
 var backWheelies = parseInt(prompt("Enter the radius for the back Wheelies"));
@@ -17,25 +20,25 @@ var land = new Land(frontWheels,frontWheels,backWheelies,backWheelies);
 
 this.speed += land.acel_wheels;
 
-alert("Final Speed: " + Math.trunc(this.speed));
+  alert("Final Speed: " + Math.trunc(this.speed));
 
-break;
+        break;
 
 case "water":
 
-alert("--Water Vehicle--");
+  alert("--Water Vehicle--");
 
 var water = new Water();
 
-water.get_propellers();
+  water.get_propellers();
 
-water.getProper_data();
+  water.getProper_data();
 
 this.speed += water.final_acel;
 
-alert("Final speed: " + this.speed);
+  alert("Final speed: " + this.speed);
 
-break;
+      break;
 
 case "air":
 
@@ -49,7 +52,7 @@ this.speed+= air_aceleration;
 
 alert("Final speed: " + this.speed);
 
-break;
+    break;
 
 case "amphibious":
 
@@ -81,121 +84,122 @@ if(type==1){
 
 }else{
   alert("You can only select between 1 or 2");
+    }
+
+    break;
+
+      default:
+
+      alert("That vehicle doesnt exist!!");
+
+      }
+
 }
-
-break;
-
-default:
-
-alert("That vehicle doesnt exist!!");
-
-}
-
-}
-
+/*Land vehicle object*/
 function Land(radius1,radius2,radius3,radius4){
 
-var radios = [radius1,radius2,radius3,radius4];
+  var radios = [radius1,radius2,radius3,radius4];
 
-var prop = new Propultion();
+  var prop = new Propultion();
 
-var name;
+  var name;
 
-for(var i=0;i<4;i++){
+    for(var i=0;i<4;i++){
 
-name = "wheel" + i;
+        name = "wheel" + i;
 
-prop.addWheel(name,radios[i]);
+        prop.addWheel(name,radios[i]);
+
+        }
+
+    this.acel_wheels = prop.get_AcelWheels();
 
 }
-
-this.acel_wheels = prop.get_AcelWheels();
-
-}
-
+/*Water vehicle object*/
 function Water(){
 
-var prop = new Propultion();
+  var prop = new Propultion();
 
-this.final_acel=0;
+  this.final_acel=0;
 
-this.get_propellers = function(){
+  this.get_propellers = function(){
 
-var prop_num = prompt("enter the number of propellers");
+    var prop_num = prompt("enter the number of propellers");
 
-var prop_number = parseInt(prop_num);
+    var prop_number = parseInt(prop_num);
 
-var prop_direction,direction,q_fin,fin_quant;
-
-
-for(var i=0;i<prop_number;i++){
-
-alert("--Propeller " + (i+1) + "--");
-
-prop_direction = prompt("Enter the direction:\n1 - clockwise\n2 - anticlockwise");
-
-q_fin = prompt("Enter the numbers of the fins");
-
-fin_quant = parseInt(q_fin);
-
-this.add_new_propeller = prop.addPropeller(fin_quant,prop_direction);
-
-}
+    var prop_direction,direction,q_fin,fin_quant;
 
 
-};
+      for(var i=0;i<prop_number;i++){
+
+          alert("--Propeller " + (i+1) + "--");
+
+          prop_direction = prompt("Enter the direction:\n1 - clockwise\n2 - anticlockwise");
+
+          q_fin = prompt("Enter the numbers of the fins");
+
+          fin_quant = parseInt(q_fin);
+
+          this.add_new_propeller = prop.addPropeller(fin_quant,prop_direction);
+
+            }
+
+
+        };
 
 this.get_dir;
 
 this.getProper_data = function(){
 
-for(var i=0;i<prop.prop_content.length;i++){
+  for(var i=0;i<prop.prop_content.length;i++){
 
-if(prop.prop_content[i].direction){
+    if(prop.prop_content[i].direction){
 
-this.get_dir="clockwise";
+      this.get_dir="clockwise";
 
-}else{
+    }else{
 
 this.get_dir = "anticlockwise";
 
-}
-alert("--Propeler " + (i+1)+"--");
-alert("direction: " + this.get_dir + " numbers of fins: " + prop.prop_content[i].fin_num);
+      }
+
+ alert("--Propeler " + (i+1)+"--");
+ alert("direction: " + this.get_dir + " numbers of fins: " + prop.prop_content[i].fin_num);
 
 this.final_acel += prop.prop_content[i].getAcelResult();
 
 alert("Propeller aceleration result: " + this.final_acel);
 
-}
-
-};
-
-}
-
-function Air(){
-
-alert("--Air Vehicle--");
-
-this.AirAceleration=0;
-
-var prop = new Propultion();
-
-this.getAceleration = function(){
-
-  var power = prompt("Enter the power of the nozlee propeller");
-  var turbo = confirm("Do you want to activate the turbo?");
-
-  prop.addNozzle(power,turbo);
-
-  this.AirAceleration=prop.getNozleePropultion();
-
-return this.AirAceleration;
+      }
 
   };
 
 }
+/*Ait vehicle object*/
+function Air(){
 
+  alert("--Air Vehicle--");
+
+  this.AirAceleration=0;
+
+    var prop = new Propultion();
+
+  this.getAceleration = function(){
+
+    var power = prompt("Enter the power of the nozlee propeller");
+    var turbo = confirm("Do you want to activate the turbo?");
+
+    prop.addNozzle(power,turbo);
+
+  this.AirAceleration=prop.getNozleePropultion();
+
+      return this.AirAceleration;
+
+  };
+
+}
+/*All Vehicles call to propultion in order to get aceleration*/
 function Propultion(){
 
 this.Finalpropultion;
@@ -206,7 +210,7 @@ this.spin_dir=false;
 this.prop_content= [];
 this.wheel_content = [];
 this.noz_content = [];
-
+/*create propelers objects for propultion uses*/
 this.addPropeller=function(finNumber,num_selection){
 
   if(num_selection==1){
@@ -222,23 +226,23 @@ var the_propeller = new Propellers(finNumber,this.spin_dir);
 
 this.prop_content.push(the_propeller);
 };
-
+/*create a wheel object for propultion uses*/
 this.addWheel = function(name,radius){
 
-var the_wheel = new Wheel(name,radius);
+  var the_wheel = new Wheel(name,radius);
 
 this.wheel_content.push(the_wheel);
 
 };
-
+/*create a nozle object for propultion uses*/
 this.addNozzle = function(power,afterburner){
 
-var nozlee = new Nozzle(power,afterburner);
+  var nozlee = new Nozzle(power,afterburner);
 
 this.noz_content.push(nozlee);
 
 };
-
+/*it return the nozlee aceleration*/
 this.getNozleePropultion=function(){
 
   var propultion = this.noz_content[0].getTurboResult();
@@ -246,23 +250,23 @@ this.getNozleePropultion=function(){
   return propultion;
 
 };
-
+/*it return the wheels aceleration*/
 this. get_AcelWheels = function(){
 
-var final_prop_wheel=0;
+  var final_prop_wheel=0;
 
-for(var i=0;i<this.wheel_content.length;i++){
+    for(var i=0;i<this.wheel_content.length;i++){
 
-final_prop_wheel += this.wheel_content[i].perimeter;
+        final_prop_wheel += this.wheel_content[i].perimeter;
 
-}
+        }
 
-return final_prop_wheel;
+          return final_prop_wheel;
 
-};
+        };
 
-}
-
+      }
+/*It give the aceleration to travel in water*/
 function Propellers(finNumber,spinDirection){
 
   this.direction=spinDirection;
@@ -286,7 +290,7 @@ function Propellers(finNumber,spinDirection){
     }
   };
 }
-
+/*It give the aceleration to travel in land*/
 function Wheel(name,radius){
 
 this.name=name;
@@ -297,7 +301,7 @@ var pi = 3.14;
 this.perimeter = 2 * pi * radius;
 
 }
-
+/*Nozle is the aceleration device for air*/
 function Nozzle(the_power,afterburner){
 
   this.power = 0;
@@ -319,5 +323,5 @@ function Nozzle(the_power,afterburner){
   };
 
 }
-
+/*Here you can select or create differents vehicles*/
 var vehicle = new Vehicle("amphibious",120);
