f2537b65967b1443e607f15b6a8cd184e874ee5f
mauro soppelsa <mauro_soppelsa@hotmail.com>
2015-08-25 09:02:01 -0300

    finishing ex1 part7

--- ex1.js
+++ ex1.js
@@ -1,66 +1,207 @@
-function Vehicle(){
+function  Vehicle(type_of_vehicle,the_speed){
 
+var type= type_of_vehicle;
 
-  this.model="";
-  this.speed;
-  this.acelerate = function(propultion){
+this.speed=the_speed;
 
-    this.speed+=propultion.Finalpropultion;
+switch(type_of_vehicle){
 
-    alert("Model: "+this.model + " - Wheels Radius: " +propultion.wheelRd+ " - Wheels propultion: " +Math.trunc(propultion.wheelsProp) + " -  Nozzles: " + propultion.nozzles + " - Propellers: " + propultion.propellers );
-    alert("The vehicle is acelerating by " + Math.trunc(propultion.Finalpropultion) + ", Final speed: " + Math.trunc(this.speed));
+case "land":
 
-  };
+var land = new Land(10,10,10,10);
 
-  this.pressTurboButton= function(propultion){
+this.speed += land.acel_wheels;
 
-    var nozzlesTurbo = propultion.nozzles * 2;
-    this.speed = this.speed + propultion.nozzles;
+alert("--Land Vehicle--");
+alert("Final Speed: " + Math.trunc(this.speed));
 
-    alert("Turbo Button was pressed in " + this.model);
-    alert("nozzles is incremented to " + nozzlesTurbo + " speed is now " + this.speed);
+break;
 
-  }
+case "water":
+
+alert("--Water Vehicle--");
+
+var water = new Water();
+
+water.get_propellers();
+
+water.getProper_data();
+
+this.speed += water.final_acel;
+
+alert("Final speed: " + this.speed);
+
+break;
+
+case "air":
+
+var air = new Air();
+
+break;
+
+default:
+
+alert("That vehicle doesnt exist!!");
 
 }
 
-function Propultion(wheelRad,finNumber,spin_dir){
+}
 
-var wheels = new Wheels(wheelRad);
+function Land(radius1,radius2,radius3,radius4){
 
-this.afterburner=false;
-this.wheelRd=wheelRad;
-this.wheelsProp = wheels.perimeter;
-this.nozzles;
-var ac_propellers = new Propellers(finNumber,spin_dir);
-this.propellers = ac_propellers.getAcelResult();
-this.Finalpropultion;
-this.addPropultion = function(nozzles,propellers){
+var radios = [radius1,radius2,radius3,radius4];
 
-this.Finalpropultion= this.wheelsProp + nozzles + propellers;
+var prop = new Propultion();
+
+var name;
+
+for(var i=0;i<4;i++){
+
+name = "wheel" + i;
+
+prop.addWheel(name,radios[i]);
+
+}
+
+this.acel_wheels = prop.get_AcelWheels();
+
+}
+
+function Water(){
+
+var prop = new Propultion();
+
+this.final_acel=0;
+
+this.get_propellers = function(){
+
+var prop_num = prompt("enter the number of propellers");
+
+var prop_number = parseInt(prop_num);
+
+var prop_direction,direction,q_fin,fin_quant,bool_dir=false;
+
+
+for(var i=0;i<prop_number;i++){
+
+prop_direction = prompt("Enter the direction:\n1 - clockwise\n2 - anticlockwise");
+
+direction = parseInt(prop_direction);
+
+q_fin = prompt("Enter the numbers of the fins");
+
+fin_quant = parseInt(q_fin);
+
+if(prop_direction=1){
+
+bool_dir = true;
+
+}else if(direction=2){
+
+bool_dir = false;
+
+}
+
+this.add_new_propeller = prop.addPropeller(fin_quant,bool_dir);
+
+}
+
+
+};
+
+this.get_dir;
+
+this.getProper_data = function(){
+
+for(var i=0;i<prop.prop_content.length;i++){
+
+if(prop.prop_content[i].direction=true){
+
+this.get_dir="clockwise";
+
+}else{
+
+this.get_dir = "anticlockwise";
+
+}
+
+alert("Propeller added:" + " direction: " + this.get_dir + " numbers of fins: " + prop.prop_content[i].fin_num);
+
+this.final_acel = prop.prop_content[i].getAcelResult();
+
+alert("Propeller aceleration result: " + this.final_acel);
+
+}
 
 };
 
 }
 
-function Wheels(radius){
+function Air(turbo_button){
 
-  var pi = 3.14;
 
-  this.perimeter = 2 * pi * radius;
+
+}
+
+function Propultion(){
+
+this.Finalpropultion;
+this.wheels;
+this.propellers;
+this.nozlees;
+
+this.prop_content= [];
+this.wheel_content = [];
+this.noz_content = [];
+
+this.addPropeller=function(finNumber,spinDirection){
+
+var the_propeller = new Propellers(finNumber,spinDirection);
+
+this.prop_content.push(the_propeller);
+};
+
+this.addWheel = function(name,radius){
+
+var the_wheel = new Wheel(name,radius);
+
+this.wheel_content.push(the_wheel);
+
+};
+
+this.addNozzle = function(nozlee){
+
+this.noz_content.push(nozlee);
+
+};
+
+this. get_AcelWheels = function(){
+
+var final_prop_wheel=0;
+
+for(var i=0;i<this.wheel_content.length;i++){
+
+final_prop_wheel += this.wheel_content[i].perimeter;
+
+}
+
+return final_prop_wheel;
+
+};
 
 }
 
 function Propellers(finNumber,spinDirection){
 
+  this.direction=spinDirection;
+  this.fin_num = finNumber;
+
   var prop_acelerate;
   this.getAcelResult = function(){
 
-    if(spinDirection){
+    if(spinDirection=true){
 
       prop_acelerate = finNumber ;
-
-      alert("the propellers are acelerating by " + finNumber + " in the vehicle");
 
       return prop_acelerate;
 
@@ -68,36 +209,45 @@ function Propellers(finNumber,spinDirection){
 
     prop_acelerate = (finNumber)*-1;
 
-    alert("the propellers are decelerating by " + finNumber + " in the vehicle");
-
     return prop_acelerate;
 
     }
   };
 }
 
-var vehicle1 = new Vehicle();
-vehicle1.speed=100;
-vehicle1.model="Camaro";
+function Wheel(name,radius){
 
-var vehicle2 = new Vehicle();
-vehicle2.speed=130;
-vehicle2.model="Ferrari";
+this.name=name;
+this.radius=radius;
+
+var pi = 3.14;
+
+this.perimeter = 2 * pi * radius;
+
+}
+
+function Nozzle(the_power){
+
+  var power=0;
+  this.afterburner=false;
+  this.getTurboResult= function(){
+
+    if(afterburner){
+
+      this.power+=the_power*2;
+
+      return power;
+
+    }else{
+
+      this.power=the_power;
+
+      return power;
+
+    }
+  };
 
 
-alert('Vehicle: ' + vehicle1.model + " Speed: " + vehicle1.speed);
+}
 
-var propultion1 = new Propultion(10,15,true);
-propultion1.nozzles=30;
-propultion1.addPropultion(propultion1.nozzles,propultion1.propellers);
-
-vehicle1.acelerate(propultion1);
-
-alert('Vehicle: ' + vehicle2.model + " Speed: " + vehicle2.speed);
-
-var propultion2 = new Propultion(15,20,false);
-propultion2.nozzles=30;
-propultion2.addPropultion(propultion2.nozzles,propultion2.propellers);
-
-vehicle2.acelerate(propultion2);
-vehicle2.pressTurboButton(propultion2);
+var vehicle = new Vehicle("water",120);
